b81a58fa69ae2632f82ace24e293e70229139a2f
shreyas463 <shreyaschaudhary3@gmail.com>
2025-09-08 22:08:47 -0500

    WIP: getting setup ready

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+  optimizeDeps: {
+    exclude: ['electron'],
+  },
+})
